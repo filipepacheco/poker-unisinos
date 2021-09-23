1010dbf6549601c825de2c473d08fa02b56d6092
fipache <filipe_pacheco@gap.com>
2021-09-22 21:41:58 -0300

    init commit

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,7 +14,7 @@ PORT = 50000
 # AF_INET: IPV4
 # SOCK_STREAM: TCP
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STEAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen()
 print("Wait for connection of client")
